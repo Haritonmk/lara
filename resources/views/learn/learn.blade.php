0c6a26631dd7136de356176121e77103a114b9b4
Igor <haritonmk@yandex.ru>
2018-02-28 09:20:27 +0100

    Revert "add funtcionali articles"
    This reverts commit d0a6437fba7b7061f68397e3a45f7508706718d4.

--- resources/views/learn/learn.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <a href="{{ url("/learn/list-category") }}" class="btn btn-default btn-lg">List</a>
-    <a href="{{ url("/learn/phrase") }}" class="btn btn-default btn-lg">Phrase</a>
-    <a href="{{ url("/learn/articles") }}" class="btn btn-default btn-lg">Articles</a>
-</div>
-@endsection
