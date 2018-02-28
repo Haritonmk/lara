0e0aca589db0629d8826fe1f3ddb421e58c78864
Igor <haritonmk@yandex.ru>
2018-02-28 09:40:56 +0100

    cancel change

--- public/forms/_topLogo.php
+++
@@ -1,7 +0,0 @@
-<?
-    if( isUserLoggedIn() && getCurrentUserType()!="admin" )
-        return;
-     
-    $logoFile = getInstanceFile("img/topLogo.png");
-    echo "<center><img src=\"$logoFile\" width=\"100%\" height=\"150\" alt=\"\"/></center>";
-?>
