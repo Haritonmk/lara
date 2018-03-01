5367a2574fdaae8da402f9a5df25841297ddf74a
Igor <haritonmk@yandex.ru>
2018-03-01 15:55:17 +0100

    update frontend

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,36 +1,34 @@
-var elixir = require('laravel-elixir');
+const {mix} = require('laravel-mix');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
-/*
-elixir(function(mix) {
-    mix.sass('app.scss');
+// paths to clean
+var pathsToClean = [
+    'public/js',
+    'public/css',
+];
+
+// the clean options to use
+var cleanOptions = {};
+
+mix.webpackConfig({
+    plugins: [
+        new CleanWebpackPlugin(pathsToClean, cleanOptions)
+    ]
 });
-*/
-elixir.config.sourcemaps = false;
-elixir(function(mix) {
 
 	mix.scripts([
 		'node_modules/jquery/dist/jquery.js',
 		'node_modules/bootstrap/dist/js/bootstrap.js',
 		'node_modules/gentelella/vendors/bootstrap-progressbar/bootstrap-progressbar.min.js',
 		'node_modules/gentelella/build/js/custom.js',
-	], 'public/js/app.js');
+	], 'public/js/app.js').version();
 
 	mix.styles([
 		'node_modules/font-awesome/css/font-awesome.css',
 		'node_modules/bootstrap/dist/css/bootstrap.css',
 		'node_modules/gentelella/vendors/animate.css/animate.css',
 		'node_modules/gentelella/build/css/custom.css',
-	], 'public/css/app.css');
+	], 'public/css/app.css').version();
 
 	mix.copy([
 		'node_modules/font-awesome/fonts/',
@@ -56,14 +54,14 @@ elixir(function(mix) {
 		'node_modules/gentelella/vendors/Chart.js/dist/Chart.js',
 
 		'resources/assets/js/app.js',
-	], 'public/js/source.js');
+	], 'public/js/source.js').version();
 
 	mix.styles([
 		'node_modules/select2/dist/css/select2.css',
 		'node_modules/gentelella/vendors/bootstrap-daterangepicker/daterangepicker.css',
 		'resources/assets/css/app.css',
-	], 'public/css/source.css');
+	], 'public/css/source.css').version();
 
-	mix.version(['js/app.js', 'css/app.css', 'js/source.js', 'css/source.css']);
-	
-});
+	mix.scripts([
+		'node_modules/vue/dist/vue.js'
+	],'public/js/vue.js').version();
