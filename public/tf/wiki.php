060d552ffaf6d3c7bf34456dd2d3c67169941833
Igor <haritonmk@yandex.ru>
2019-12-18 15:11:25 +0100

    test files

---
+++ public/tf/wiki.php
@@ -0,0 +1,26 @@
+﻿<?php 
+$handle = fopen("tags.txt", "r");
+?>
+<html>
+<body>
+<table>
+<tbody>
+<?
+var $rowBr = false;
+while (!feof($handle)) {
+	if(!$rowBr){
+		echo "<tr>"
+	}
+    $buffer = fgets($handle);
+    echo "<td>".$buffer."</td>";
+	if($rowBr){
+		echo "</tr>"
+	}
+	$rowBr = !$rowBr;
+}
+fclose($handle);
+?>
+</tbody>
+</table>
+</body>
+</html>
